82e7485f45c083b2ce5f49269d8b010dfcbd27a8
jailant <seba0116641295>
2019-11-01 18:50:50 -0300

    Added models and migrations of  seat and genre.

--- database/migrations/1572640838857_booking_schema.js
+++ database/migrations/1572640838857_booking_schema.js
@@ -7,10 +7,10 @@ class BookingSchema extends Schema {
   up () {
     this.create('bookings', table => {
       table.increments()
-      table.integer('customer_id').unsigned().references('customers.id')
-      table.integer('movie_showing_times_id').unsigned().references('movie_showing_times.id')
+      table.integer('customer_id').unsigned().references('customers.id').comment('What movie will they see')
+      table.integer('movie_showing_times_id').unsigned().references('movie_showing_times.id').comment('When it has been done')
       table.datetime('booking_made_date')
-      table.integer('booking_seat_count')
+      table.integer('booking_seat_count').comment('Number of seats booked')
     })
   }
 
